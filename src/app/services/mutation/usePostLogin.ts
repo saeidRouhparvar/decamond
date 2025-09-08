5228856f3d050b0cd97dea4361948a67b788dfa0
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-09-09 01:37:14 +0330

    refactor usePostLogin

--- src/app/services/mutation/usePostLogin.ts
+++ src/app/services/mutation/usePostLogin.ts
@@ -1,12 +1,8 @@
 import axiosApiInstance from "@/app/api/axiosConfig";
 import { endpoints } from "@/app/api/configs/mainConfig";
-import { LoginForm } from "@/app/lib/types";
+import {  LoginResponseType } from "@/app/lib/types";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-
-export type LoginResponseType = {
-  data: LoginForm;
-};
 
 export const fetchRandomUser = async (): Promise<LoginResponseType> => {
   const response = await axiosApiInstance.get<LoginResponseType>(endpoints.login);
