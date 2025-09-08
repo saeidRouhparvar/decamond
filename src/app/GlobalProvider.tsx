3282e734c7ec2f8c61507bcc83211cc249a8cbcd
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-09-09 02:29:28 +0330

    refactor global provider

--- src/app/GlobalProvider.tsx
+++ src/app/GlobalProvider.tsx
@@ -1,22 +1,25 @@
-"use client";
+'use client';
 
 import React, { FC, ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import MainLayout from "./mainLayout";
+import { usePathname } from "next/navigation";
 
 type GlobalProviderProps = {
-    children: ReactNode;
+  children: ReactNode;
 };
 
-const queryClient = new QueryClient();
-
 export const GlobalProvider: FC<GlobalProviderProps> = ({ children }) => {
- 
-    return (
-        <QueryClientProvider client={queryClient}>
-           <MainLayout>{children}</MainLayout>
-            <Toaster position="top-center" />
-        </QueryClientProvider>
-    );
+  const pathname = usePathname();
+  const isLoginPage = pathname === "/login";
+
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {!isLoginPage ? <MainLayout>{children}</MainLayout> : children}
+      <Toaster position="top-center" />
+    </QueryClientProvider>
+  );
 };
