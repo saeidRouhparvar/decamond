9ee2c9c20cef2b5e058fed85009fb6107c55392c
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-09-09 01:40:18 +0330

    add new types

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -1,4 +1,20 @@
 export type LoginForm = {
-    phone:string
-}
+  phone: string;
+};
 
+export type LoginResponseType = {
+  results: Array<{
+    name: {
+      title: string;
+      first: string;
+      last: string;
+    };
+    email: string;
+    phone: string;
+    picture: {
+      large: string;
+      medium: string;
+      thumbnail: string;
+    };
+  }>;
+};
