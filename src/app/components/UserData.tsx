e4a91550fe2502a23e3643745f326076db2222cf
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-09-09 01:39:10 +0330

    create userDate

---
+++ src/app/components/UserData.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react'
+import Avatar from './Avatar'
+type UserDataProps = {
+    picture: string
+    alt: string
+    email: string
+    name: string
+}
+const UserData: FC<UserDataProps> = ({ picture, alt, email, name }) => {
+    return (
+        <div className='flex items-center gap-4'>
+            <Avatar src={picture} alt={alt} />
+            <div className="flex flex-col gap-1 text-neutral-800 lg:text-md sm:text-sm">
+                <span className="">{name}</span>
+                <span className="">{email}</span>
+            </div>
+        </div>
+    )
+}
+
+export default UserData
