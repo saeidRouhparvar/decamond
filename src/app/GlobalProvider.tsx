43c9af76473d91aa50f54de5a4da2a289a7ea1c9
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-09-09 01:35:24 +0330

    ad globalProvider

---
+++ src/app/GlobalProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { FC, ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import MainLayout from "./mainLayout";
+import { usePathname } from "next/navigation";
+
+type GlobalProviderProps = {
+    children: ReactNode;
+};
+
+const queryClient = new QueryClient();
+
+export const GlobalProvider: FC<GlobalProviderProps> = ({ children }) => {
+    const pathname = usePathname();
+
+    const isLoginPage = pathname === "/login";
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {!isLoginPage ? <MainLayout>{children}</MainLayout> : children}
+            <Toaster position="top-center" />
+        </QueryClientProvider>
+    );
+};
