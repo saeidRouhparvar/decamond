6564b154f727ba1cf4d70b3d1aaa7e14e7d8e9df
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-09-09 11:59:06 +0330

    add new svg

--- src/app/components/UserData.tsx
+++ src/app/components/UserData.tsx
@@ -7,13 +7,14 @@ type UserDataProps = {
     email: string
     name: string
 }
+
 const UserData: FC<UserDataProps> = ({ picture, alt, email, name }) => {
     return (
-        <div className='flex items-center gap-4'>
+        <div className='flex items-center gap-2 sm:gap-1 md:gap-3'>
             <Avatar src={picture} alt={alt} />
-            <div className="flex flex-col gap-1 text-neutral-800 lg:text-md sm:text-sm">
-                <span className="">{name}</span>
-                <span className="">{email}</span>
+            <div className="flex flex-col text-neutral-800">
+                <span className="text-sm sm:text-xs md:text-base lg:text-lg font-medium">{name}</span>
+                <span className="text-xs sm:text-[10px] md:text-sm lg:text-md text-neutral-600">{email}</span>
             </div>
         </div>
     )
--- src/app/components/svg.tsx
+++ src/app/components/svg.tsx
@@ -1,3 +1,13 @@
 export const close = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" viewBox="0 0 20 18" fill="none">
     <path d="M15 4L13.59 5.41L16.17 8H6V10H16.17L13.59 12.58L15 14L20 9L15 4ZM2 2H10V0H2C0.9 0 0 0.9 0 2V16C0 17.1 0.9 18 2 18H10V16H2V2Z" fill="#2F0D68" />
 </svg>
+
+export const burger = <svg
+    className="w-6 h-6"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+</svg>
