40cad0e256647675a91b6ee17783a3ba645ae67d
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-09-09 11:57:19 +0330

    add new ui components to index export

--- src/app/components/ui/index.ts
+++ src/app/components/ui/index.ts
@@ -1,4 +1,5 @@
-export { default as Button } from "./button"
-export { default as Input } from "./input"
+export { default as Button } from "./Button"
+export { default as Input } from "./Input"
 export { default as Avatar } from "./Avatar"
 export { default as FadeLoader } from "./FadeLoader"
+export { default as Navigator } from "./Navigator"
