4c5834bd7f42462d068b730ff2e04162955591b8
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-09-09 11:58:33 +0330

    add responsive to sidebar

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,55 +1,44 @@
 'use client'
 
-import React, { useState } from 'react'
-import Modal from './Modal'
+import React, { Dispatch, FC, SetStateAction } from 'react'
 import { close } from './svg'
-import { useRouter } from 'next/navigation'
-import Button from './ui/button'
+import Image from 'next/image'
+import Logo from '../../../public/logo.png'
+import { Navigator } from './ui'
 
-const SideBar = () => {
-    const [closeModal, setCloseModal] = useState(false)
-    const router = useRouter()
+type SideBarProps = {
+    setCloseModal: Dispatch<SetStateAction<boolean>>
+    setSidebarOpen: Dispatch<SetStateAction<boolean>>
+}
 
-    const closeHandler = () => {
-        localStorage.removeItem('userData')
-        router.replace('/login')
-        setCloseModal(true)
-    }
+const SideBar: FC<SideBarProps> = ({ setCloseModal,setSidebarOpen }) => {
 
     return (
-        <div>
-            {/* Footer bar */}
-            <div className="fixed bottom-0 p-6">
-                <div className="flex items-center gap-4 w-full">
+        <div className='flex p-4 flex-col items-center'>
+            <div className="absolute top-1 right-2 text-xl
+             text-violet-950 cursor-pointer hover:bg-violet-200
+             px-2 rounded-lg sm:hidden"
+             onClick={()=> setSidebarOpen(false)}>X</div>
+            <div className="mb-12">
+                <Image src={Logo} alt='logo' className='h-16 w-16' />
+            </div>
+
+            <div className="w-full">
+                <Navigator title='Dashboard' />
+            </div>
+
+            {/* Footer Logout */}
+            <div className="fixed bottom-0 p-4 sm:p-6 w-full">
+                <div className="flex items-center gap-2 sm:gap-4 ">
                     <span>{close}</span>
                     <div
-                        className="text-violet-950 cursor-pointer sm:text-md md:text-lg lg:text-xl"
+                        className="text-violet-950 cursor-pointer text-sm sm:text-md md:text-lg lg:text-xl"
                         onClick={() => setCloseModal(true)}
                     >
                         Logout
                     </div>
                 </div>
             </div>
-
-            {/* Logout confirmation modal */}
-            <Modal
-                isOpen={closeModal}
-                onClose={() => setCloseModal(false)}
-            >
-                <div className="flex flex-col gap-6 sm:gap-8">
-                    <p className='sm:text-lg md:text-xl lg:text-2xl'>
-                        Are you sure you want to log out?
-                    </p>
-                    <div className="flex justify-between">
-                        <div className=""></div>
-                        <div className="flex gap-4">
-                            <Button variant='outline' onClick={() => setCloseModal(false)}>Cancel</Button>
-                            <Button onClick={closeHandler}>Logout</Button>
-                        </div>
-                    </div>
-                </div>
-
-            </Modal>
         </div>
     )
 }
