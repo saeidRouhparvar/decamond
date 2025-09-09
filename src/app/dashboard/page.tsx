50e996fcd1d55e4e7af4bb6bd6968eb6d439ed51
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-09-09 11:54:26 +0330

    add dashboard illus

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,11 @@
 'use client'
 
+import Image from 'next/image'
 
 const Dashboard = () => {
-
-
     return (
-        <div className="">
-           
+        <div className="grid place-content-center">
+            <Image src="/illus.png" alt='dashboard' width={500} height={500} priority/>
         </div>
     )
 }
