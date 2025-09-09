6aa097ad0185bebfabee73ed899d73efc31f60c7
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-09-09 12:19:46 +0330

    refactor base url

--- src/app/api/configs/mainConfig.ts
+++ src/app/api/configs/mainConfig.ts
@@ -1,12 +1,5 @@
-type EnvironmentT = "develop" | "production";
-
-// Retrieve environment variable
-const environment: EnvironmentT =
-  (process.env.NEXT_PUBLIC_ENVIRONMENT as EnvironmentT) || "develop";
-
 // Base URL based on environment
-export const baseUrl: string =
-  environment === "production" ? "https://randomuser.me" : "";
+export const baseUrl: string = "https://randomuser.me";
 
 // API Endpoints
 export const endpoints = {
